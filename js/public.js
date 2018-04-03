392bf3129740c7641e85481efa62e32dca5e06f3
zhongRui_HE <418351897@qq.com>
2018-04-03 08:39:44 +0800

    2018-04-03 8.39

--- js/public.js
+++ js/public.js
@@ -3,4 +3,27 @@ $(function () {
         $(this).addClass('sel');
         $(this).siblings('li').removeClass('sel');
     })
+    $('.bottom .weixin').hover(
+        function () {
+            $('#wx-icon').attr({
+                src:'img/wenxinsel.png'
+            })
+        },
+        function () {
+            $('#wx-icon').attr({
+                src:'img/wenxin.png'
+            })
+        }
+    )
+    $('.toTop').click(function () {
+        $('html , body').animate({scrollTop: 0},'slow');
+    })
+    $(window).scroll(function (e) {
+        console.log($(document).scrollTop())
+        if($(document).scrollTop()>1800){
+            $('.bottom').addClass('d-block')
+        }else{
+            $('.bottom').removeClass('d-block')
+        }
+    })
 })
