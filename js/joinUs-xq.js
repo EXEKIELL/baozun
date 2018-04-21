7f11ef54433cebe3de44d608f88d2c86f7cd7b41
zhongRui_HE <418351897@qq.com>
2018-04-21 14:51:01 +0800

    2018-04-21 14.50

--- js/joinUs-xq.js
+++ js/joinUs-xq.js
@@ -7,4 +7,55 @@ $(function () {
         console.log(1)
         window.location.href="加入保尊.html";
     })
+    $('#shenqing').click(function () {
+        $('#shenqingWrap').css({
+            "display":"flex"
+        })
+    })
+    $('#tijiaoBtn').click(function () {
+        $('#shenqingWrap').css({
+            "display":"none"
+        })
+    })
+    $('.shenqingWrite input').focusin(function (event) {
+        if($(this).parent('.shenqingWrite').index()==0){
+            $(this).parent('.shenqingWrite').css({
+                "border":"1px solid #1471c2"
+            })
+            $(event.target).parent('.shenqingWrite').find('span').addClass('num2')
+        }else if($(this).parent('.shenqingWrite').index()==1){
+            $(this).parent('.shenqingWrite').css({
+                "border":"1px solid #1471c2"
+            })
+            $(event.target).parent('.shenqingWrite').find('span').addClass('num2')
+        }
+    })
+    $('.shenqingWrite input').focusout(function (event){
+        if($(this).parent('.shenqingWrite').index()==0){
+            $(this).parent('.shenqingWrite').css({
+                "border":"1px solid #c8c8c8"
+            })
+            $(event.target).parent('.shenqingWrite').find('span').removeClass('num2').addClass('num1')
+        }else if($(this).parent('.shenqingWrite').index()==1){
+            $(this).parent('.shenqingWrite').css({
+                "border":"1px solid #c8c8c8"
+            })
+            $(event.target).parent('.shenqingWrite').find('span').removeClass('num2').addClass('num1')
+        }
+    })
+    $('.shenqingWrite textarea').focusin(function () {
+        $(this).parent('div').css({
+            border:"1px solid #1471c2"
+        })
+    })
+    $('.shenqingWrite textarea').focusout(function () {
+        $(this).parent('div').css({
+            border:"1px solid #c8c8c8"
+        })
+    })
+    $('.closeShenqing').click(function () {
+        $('#shenqingWrap').css({
+            "display":"none"
+        })
+    })
 })
